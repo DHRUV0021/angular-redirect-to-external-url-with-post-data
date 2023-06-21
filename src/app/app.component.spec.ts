19a2a7ec8609e022de345ebe64301a00a392bb99
dhruv.g@gurukul.org <dhruv.g@gurukul.org>
2023-06-21 16:27:29 +0530

    Angular Redirect Url

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'edu'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('edu');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'edu';
+
+  postAndRedirect() {
+    const data = {
+      key: '57d9d47d-f205-4f2a-8f3a-a08c5197824z',
+      admissionNo: 'AF0846'
+    }
+
+    var formData: any = new FormData();
+    formData.append("key", "57d9d47d-f205-4f2a-8f3a-a08c5197824z");
+    formData.append("admissionNo", "AF0846");
+
+
+    // const targetUrl = 'https://swaadhyayan.com/school/ErpController/erplogin/student';
+
+    const myform = document.createElement('form');
+    const mapInput = document.createElement('input');
+    mapInput.type = 'hidden';
+    mapInput.name = 'key';
+    mapInput.value = '57d9d47d-f205-4f2a-8f3a-a08c5197824z';
+    myform.appendChild(mapInput);
+
+    const mapInput1 = document.createElement('input');
+    mapInput1.type = 'hidden';
+    mapInput1.name = 'admissionNo';
+    mapInput1.value = 'AF0846';
+    myform.appendChild(mapInput1);
+
+
+    myform.method = 'POST';
+    myform.action = "https://swaadhyayan.com/school/ErpController/erplogin/student";
+    myform.style.display = 'none';
+    myform.append('Content-Type', 'application/x-www-form-urlencoded');
+    myform.append('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8');
+    document.body.appendChild(myform);
+    myform.submit();
+
+  }
 }
